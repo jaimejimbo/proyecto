3f595313b6e23601fc4ee4f93446a576320e3ff9
Jaime Pérez Aparicio <19.jaime.91@gmail.com>
2015-05-01 10:44:08 +0200

    Cambios: - Ayudas con macros para ahorrar tiempo.

--- ejecutame.sh
+++ ejecutame.sh
@@ -3,11 +3,3 @@
 make
 ./proyecto 0
 gnuplot graphs.plot
-./proyecto 10
-gnuplot graphs.plot
-./proyecto 100
-gnuplot graphs.plot
-./proyecto 1000
-gnuplot graphs.plot
-./proyecto 10000
-gnuplot graphs.plot
--- main.cc
+++ main.cc
@@ -8,6 +8,12 @@
 using namespace std;
 #include "metropolis.hpp"
 
+//#define ESTADOS2
+//#define ESTADOS3
+#define ESTADOS4
+#define PROGRESIVO
+
+
 void set_ceros(double **matriz, int filas, int columnas);
 void antisimetriza(double **matriz, int filas, int columnas);
 
@@ -15,7 +21,15 @@ int main(int argc, char** argv){
   double temp=0;
   if (argc==2) temp=(double)atof(argv[1]);
   //parametros
+#ifdef ESTADOS2
+  const int N_posibles_estados = 2;
+#endif
+#ifdef ESTADOS3
   const int N_posibles_estados = 3;
+#endif
+#ifdef ESTADOS4
+  const int N_posibles_estados = 4;
+#endif
   string posibles_estados[N_posibles_estados];
   double **influencia_vecinos = new double*[N_posibles_estados];
   for (int i=0; i<N_posibles_estados; i++)
@@ -29,27 +43,58 @@ int main(int argc, char** argv){
   const int size_x = 100;
   const int size_y = 100;
   modelo<size_y,size_x,string> modelo1;
-  posibles_estados[0] = "egoista";
-  posibles_estados[1] = "altruista";
-  posibles_estados[2] = "precavido";
+#ifdef ESTADOS2
+  posibles_estados[0] = "altruista";
+  posibles_estados[1] = "egoista";
+#endif
+#ifdef ESTADOS3
+  posibles_estados[0] = "altruista";
+  posibles_estados[1] = "egoista";
+  posibles_estados[2] = "copia";
+#endif
+#ifdef ESTADOS4
+  posibles_estados[0] = "altruista";
+  posibles_estados[1] = "egoista";
+  posibles_estados[2] = "copia";
+  posibles_estados[3] = "contrario";
+#endif
   modelo1.definir_posibles_estados(N_posibles_estados, posibles_estados);
   modelo1.llenar();
   modelo1.set_temp(temp);
   modelo1.set_kb(1);
   modelo1.set_A_prob(1);
-  modelo1.set_influencia_externa(0);
-  modelo1.set_condicion_externa("precavido");
-  influencia_vecinos[0][0] = -10;
-  influencia_vecinos[1][1] = 5;
-  influencia_vecinos[0][1] = 20;
-  influencia_vecinos[0][2] = 0;
-  influencia_vecinos[1][2] = 0;
-  influencia_vecinos[2][2] = -1000;
+  modelo1.set_influencia_externa(5);
+#ifdef ESTADOS2
+  modelo1.set_condicion_externa("altruista");
+  influencia_vecinos[0][0] = 10;
+  influencia_vecinos[1][1] = -5;
+  influencia_vecinos[0][1] = -20;
+#endif
+#ifdef ESTADOS3
+  modelo1.set_condicion_externa("altruista");
+  influencia_vecinos[0][0] = 10;
+  influencia_vecinos[1][1] = -5;
+  influencia_vecinos[2][2] = 0;
+  influencia_vecinos[0][1] = -20;
+  influencia_vecinos[0][2] = 10;
+  influencia_vecinos[1][2] = -5;
+#endif
+#ifdef ESTADOS4
+  modelo1.set_condicion_externa("copia");
+  influencia_vecinos[0][0] = 10;
+  influencia_vecinos[1][1] = -5;
+  influencia_vecinos[2][2] = 0;
+  influencia_vecinos[3][3] = 0;
+  influencia_vecinos[0][1] = -20;
+  influencia_vecinos[0][2] = 10;
+  influencia_vecinos[1][2] = -5;
+  influencia_vecinos[0][3] = -20;
+  influencia_vecinos[1][3] = 20;
+  influencia_vecinos[2][3] = 0;
+#endif
   antisimetriza(influencia_vecinos,N_posibles_estados,N_posibles_estados);
-  influencia_vecinos[2][1] = -1000;
-  influencia_vecinos[2][0] = -1000;
   const int longitud_paso=10;
-  const int numero_pasos=250;
+  const int numero_pasos=10000;
   modelo1.set_influencia_primeros_vecinos(influencia_vecinos);
 
 
@@ -80,9 +125,15 @@ int main(int argc, char** argv){
   int* num_estados;
   cout<<"\n";
   int k=-1;
+#ifdef PROGRESIVO
+  int hk = -1;
+#endif
   for (int i=0; i<numero_pasos; i++) 
   {
 	k++;
+#ifdef PROGRESIVO
+        hk++;
+#endif
 	modelo1.cambiar_estado();
         if (k==longitud_paso){
 		modelo1.contar_estados();
@@ -96,6 +147,22 @@ int main(int argc, char** argv){
 		energia_t<<i<<"\t"<<modelo1.energia()<<"\n";
                 k=0;
 	}  
+	if (hk==100){
+#ifdef PROGRESIVO
+	  influencia_vecinos[0][0] = rand()%50-100;
+	  influencia_vecinos[1][1] = rand()%50-100;
+	  influencia_vecinos[2][2] = rand()%50-100;
+	  influencia_vecinos[3][3] = rand()%50-100;
+	  influencia_vecinos[0][1] = rand()%50-100;
+	  influencia_vecinos[0][2] = rand()%50-100;
+	  influencia_vecinos[1][2] = rand()%50-100;
+	  influencia_vecinos[0][3] = rand()%50-100;
+	  influencia_vecinos[1][3] = rand()%50-100;
+	  influencia_vecinos[2][3] = rand()%50-100;
+	  antisimetriza(influencia_vecinos,N_posibles_estados,N_posibles_estados);
+	  modelo1.set_influencia_primeros_vecinos(influencia_vecinos);
+#endif
+	}
   }
 
   energia_t.close();
